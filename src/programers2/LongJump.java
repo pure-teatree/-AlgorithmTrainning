bfae276cf7c03acca72eda70a372357a917dea25
pure-teatree <rldms5120@gmail.com>
2020-10-13 22:28:19 +0900

    complete 멀리뛰기 문제

--- src/programers2/LongJump.java
+++ src/programers2/LongJump.java
@@ -12,29 +12,37 @@ package programers2;
 여기에 1234567를 나눈 나머지를 리턴하는 함수, solution을 완성하세요. 예를 들어 4가 입력된다면, 5를 return하면 됩니다.
 - n은 1 이상, 2000 이하인 정수입니다.
  */
-
+//피보나치 수열 풀이
+//n=1,1 ->1a
+//n=2,1 1,2 ->2b
+//n=3, 1 1 1,1 2,2 1 ->3
+//n=4, 1 1 1 1,1 1 2,1 2 1,2 1 1,2 2 ->5
+//n=5, 3+5=8
+//n=6, 5+8=13
+//n일때 F(n-1)+F(n-2)
 public class LongJump {
 
 	public static long solution(int n) {
         long answer = 0;
-        int start = 0;
-        int sum = 0;
-        int cnt = 1;//1로만 합한 경우 포함
-        while(start<n) {
-        	for(int i=0;i<n;i++) {
-        		sum=(1*i)+(2*(n-i));
-        		if(sum==n) {
-        			cnt++;
-        			continue;
-        		}
-        	}
-        	start++;
+        if(n<=3) {
+        	return n;
         }
+        long a = 1; //(n-2)값
+        long b = 2;//(n-1)값
+        for(int i =3;i<=n;i++) { 
+        	answer = (a+b)%1234567; 
+        	a=b%1234567;
+        	b=answer; //n의 값 = (n-2)의 값 + (n-2)의 값(a+b)
+//        	System.out.println(i+"------");
+//        	System.out.println("n-1:"+a);
+//        	System.out.println("n-2:"+b);
+        }
+        
         return answer;
     }
 	
 	public static void main(String[] args) {
-		int n = 4;
+		int n = 2000;
 		System.out.println(solution(n)); 
 
 	}
