807786327703537492fbfd7f2a2fef953262b45a
pure-teatree <rldms5120@gmail.com>
2020-10-15 22:36:41 +0900

    complete 구명보트

--- src/programers2/Lifeboat.java
+++ src/programers2/Lifeboat.java
@@ -4,24 +4,22 @@ import java.util.Arrays;
 
 public class Lifeboat {
 	public static int solution(int[] people, int limit) {
+		Arrays.sort(people); //오름차순정렬 
 		int answer = 0;
-		Arrays.sort(people);
-		int sum=0;
-        for(int i=0;i<people.length/2;i++) {
-        	sum = people[i]+people[people.length-(i+1)];
-        	if(sum>=limit) {
-        		answer++;
-        		
-        	}
-        }
-        
-        System.out.println("최종합:"+sum);
+		
+		int min = 0; //최소값
+		for(int max=people.length-1;min<=max;max--){//최대값이 줄어든다.
+			if(people[min]+people[max]<=limit) {//최소값과 최대값의 합이 같거나 작을 때 구명보트 수 증가, 다음 최소값으로 넘어감. 
+				min++; //다음 인덱스
+			}
+			answer++; //합이 if문에 걸리거나 걸리지 않으면 구명보트 수 증가
+		}
   
 		return answer;
 	}
 
 	public static void main(String[] args) {
-		int[] people = { 70, 50, 80, 50};
+		int[] people = { 70, 50, 80, 50};//50,50,70,80
 		int limit = 100;
 		System.out.println(solution(people, limit));
 
